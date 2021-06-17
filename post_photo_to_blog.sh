e34e78fa849deff86291721b6689f9242c9ee655
Finn Le Sueur <finn@lesueur.nz>
2021-06-18 09:45:22 +1200

    Adds photo posting script
    Former-commit-id: 3d8e55d950bf25b25fff86366aeff3966aa9e2e7 [formerly d3320720caec17d071f713dc02cda4c56ec2f7f9]
    Former-commit-id: bb5a9afe1f0549020188606348857d41a6b6970a

---
+++ post_photo_to_blog.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Parsing: $1"
+
+cd "/Users/finnlesueur/Git/blog/"
+git pull origin master
+
+# Date
+dateStr=$(exiftool -time:FileCreateDate -s -S "$1")
+photoDate=$(date -jf "%Y:%m:%d %T" "$dateStr")
+formattedDate=$(gdate --date="$photoDate" +"%Y-%m-%dT%T%:z")
+ymd=$(gdate --date="$photoDate" +"%Y-%m-%d")
+echo "Taken: $formattedDate"
+
+# Photographer
+read -p 'Photographer: [Finn Le Sueur]' photographer
+photographer=${photographer:-"Finn Le Sueur"}
+echo "Photographer: $photographer"
+
+read -p "Title: " title
+read -p "Description: " description
+echo ""
+
+file="+++
+title = '$title'
+date = $formattedDate
+photoDate = $formattedDate
+image = $1
+photographer = '$photographer'
++++
+
+$description
+"
+
+folderPath="/Users/finnlesueur/Git/blog/content/photos/$ymd"
+echo "Folder $folderPath";
+
+if [ -d "$folderPath" ] 
+then
+    echo "Folder doesn't exist";
+    mkdir -p "$folderPath"
+    if [ $? -eq 0 ]; then
+        echo OK
+    else
+        echo FAIL
+    fi
+else
+    echo "Folder does exist, making a second one."
+    folderPath="$folderPath-1"
+    echo "$folderPath"
+    mkdir -p "$folderPath"
+    if [ $? -eq 0 ]; then
+        echo OK
+    else
+        echo FAIL
+    fi
+fi
+
+
+cp $1 "$folderPath/"
+echo "$file" >> "$folderPath/index.md";
+
+git add .
+git commit -m "Post photo '$title' to blog."
+git push origin master
