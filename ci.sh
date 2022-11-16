26dc551d3f7f4ece6f5584bec23a9b230ba89018
Finn Le Sueur <finn@lesueur.nz>
2022-11-16 21:41:58 +1300

    CI: Continue to improve output
    Former-commit-id: c0eee617b01e77054438c1185e046ce84d060a4f [formerly 9efee7c71a4c6fd93283cd8d6a7bb14d3f2c65d5]
    Former-commit-id: e1fb49e5a79c09934100915e66139432830b957f

--- ci.sh
+++ ci.sh
@@ -6,6 +6,7 @@ set -o pipefail  # don't hide errors within pipes
 
 on_error(){
 	curl \
+		--silent \
 		--form-string "t=Blog CI Failure" \
 		--form-string "m=finn.lesueur.nz CI has failed." \
 		--form-string "d=59496" \
@@ -21,7 +22,7 @@ git fetch
 localHash=$(cat .git/refs/heads/master)
 remoteHash=$(cat .git/refs/remotes/origin/master)
 
-printf "=== Comparing ===\nLocal  Hash: %s\nRemote Hash: %s\n" "$localHash" "$remoteHash"
+# printf "=== Comparing ===\nLocal  Hash: %s\nRemote Hash: %s\n" "$localHash" "$remoteHash"
 
 if [[ "$localHash" != "$remoteHash" ]]; then
 	# Enter Python3 venv
@@ -32,25 +33,23 @@ if [[ "$localHash" != "$remoteHash" ]]; then
 	git pull origin master
 	
 	# Rebuild gpx-->json data files
-	python3 parse_gpx.py
-
-	# Remove last build
-	# rm -r "$HOME/CI/blog-build"
-	# mkdir -p "$HOME/CI/blog-build"
+	time --format="parse_gpx.py: %es" \
+		python3 parse_gpx.py
 
 	# Build site with Hugo
-	./hugo \
+	time --format="hugo: %es" \
+		./hugo \
 		--config="config.toml" \
 		--verbose \
 		--destination="$HOME/CI/blog-build/"
 
 	# Sync build to server
-	rsync \
+	time --format="rsync: %es" \
+		rsync \
 		--archive \
 		--compress \
 		--delete \
 		--stats \
-		--itemize-changes \
 		--chown=www-data:www-data \
 		--rsh="ssh -p29163" \
 		"$HOME/CI/blog-build/" \
@@ -62,6 +61,7 @@ if [[ "$localHash" != "$remoteHash" ]]; then
 	# Send success notification to phone
 	message=$(cat "$HOME/CI/blog-logfile-$(date +'%Y-%m-%d').log")
 	curl \
+		--silent \
 		--form-string "t=Blog Rebuilt" \
 		--form-string "m=$message" \
 		--form-string "d=59496" \
