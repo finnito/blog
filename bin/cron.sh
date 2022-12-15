1d8529abbc6d92f56d6535106130fbe6becdab64
Finn Le Sueur <finn@lesueur.nz>
2022-12-15 16:54:17 +1300

    CI: Make a cron file

---
+++ bin/cron.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+readonly PROGNAME=$(basename $0) # File name
+readonly PROGBASENAME=${PROGNAME%.*} # File name, without the extension
+readonly PROGDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) # File directory
+readonly ARGS="$@" # Arguments
+readonly ARGNUM="$#" # Arguments number
+
+status=$(cat /volume1/web/webhook.status)
+if [[ "$status" != "1" ]]; then
+	exit 1
+fi
+
+pkill -f ci.sh
+
+bash /volume1/ContinuousIntegration/blog/bin/ci.sh --retry
+
+echo '0' > '/volume1/web/webhook.status'
